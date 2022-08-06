adf84267b69a262b1c42617fee49199ba0b9644b
Andres Turano <turamdq@gmail.com>
2022-08-05 21:45:19 -0300

    first commit

---
+++ src/main/java/com/portfolio/turano/TuranoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.turano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuranoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuranoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/turano/TuranoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.turano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuranoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
