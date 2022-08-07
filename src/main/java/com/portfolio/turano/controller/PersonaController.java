4a6336ff7a6c089a1c154d3128d893c939af1235
Andres Turano <turamdq@gmail.com>
2022-08-07 03:05:21 -0300

    Persona terminado falta update

---
+++ src/main/java/com/portfolio/turano/controller/PersonaController.java
@@ -0,0 +1,61 @@
+package com.portfolio.turano.controller;
+
+import com.portfolio.turano.models.Persona;
+import com.portfolio.turano.services.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//ACLARO QUE ES UN CONTROLADOR CON LA ANNOTATION:
+@RestController
+public class PersonaController {
+    
+    @Autowired
+    private IPersonaService persoServ;
+    
+    //ANTE UNA SOLICITUD GET EN LA URL INDICADA
+    @GetMapping ("/ver/personas")
+    @ResponseBody
+    public List<Persona> verPersonas () {
+        return persoServ.verPersonas();    
+    }
+    
+    //ANTE UNA SOLICITUD POST EN LA URL INDICADA
+    //OBTENGO EL CONTENIDO DEL BODY Y LO GUARDO EN LA VARIABLE pers PARA GUARDAR
+    @PostMapping ("/nueva/persona")
+    public void crearPersona (@RequestBody Persona per) {
+        persoServ.crearPersona(per);    
+    }
+    
+    //ANTE UNA SOLICITUD DELETE EN LA URL INDICADA BORRO POR ID LA PERSONA
+    //CON PathVariable RECIBO EN LA URL UNA VARIABLE (ID)
+    @DeleteMapping ("/borrar/{id}")
+    public void borrarPersona (@PathVariable Long id) {
+        persoServ.borrarPersona(id);
+    }
+    
+    //ANTE UNA SOLICITUD PUT EN LA URL INDICADA EDITO POR ID LA PERSONA
+    @PutMapping ("editar/{id}")
+    public Persona editarPersona (@PathVariable Long id,
+                               @RequestParam("name") String newName,
+                               @RequestParam("position") String newPosition,
+                               @RequestParam("about") String newAbout){
+        Persona per = persoServ.buscarPersona(id);
+        
+        per.setName(newName);
+        per.setPosition(newPosition);
+        per.setAbout(newAbout);
+        
+        persoServ.crearPersona(per);
+        return per;
+    }
+            
+}
--- src/main/java/com/portfolio/turano/services/PersonaService.java
+++ src/main/java/com/portfolio/turano/services/PersonaService.java
@@ -1,5 +1,41 @@
 package com.portfolio.turano.services;
 
-public class PersonaService {
+import com.portfolio.turano.models.Persona;
+import com.portfolio.turano.repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//IMPLEMENTA LOS METODOS DE IPersonaService (ACA IMPLEMENTAMOS LOGICA DE NEGOCIO)
+//AVISO QUE ES UNA CAPA DE SERVICIO
+@Service
+public class PersonaService implements IPersonaService{
+
+    //INYECTO DEPENDENCIAS DE FORMA AUTO
+    @Autowired
+    public IPersonaRepository persoRepo;
     
+    @Override
+    public List<Persona> verPersonas() {
+        return persoRepo.findAll();
+    }
+
+    @Override
+    public void crearPersona(Persona per) {
+        persoRepo.save(per);
+    }
+
+    @Override
+    public void borrarPersona(Long id) {
+        persoRepo.deleteById(id);
+    }
+
+    //SE COLOCA orElse(null) POR SI NO ENCUENTRA LA PERSONA
+    @Override
+    public Persona buscarPersona(Long id) {
+       return persoRepo.findById(id).orElse(null);
+    }
+    
+    /*NO SE COLOCA UN METODO EDITAR YA QUE HIBERNATE UTILIZA SAVE PARA
+    MODIFICAR O EDITAR SI ES QUE HAY CAMBIOS*/
 }
