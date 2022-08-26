85eda52a129bd23f78e8da1b70dc597d8ba80e2f
Andres Turano <turamdq@gmail.com>
2022-08-26 00:57:53 -0300

    Cors cerregidos

--- src/main/java/com/portfolio/turano/controller/HeaderController.java
+++ src/main/java/com/portfolio/turano/controller/HeaderController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="https://frontendturano.web.app")
 public class HeaderController {
     
     @Autowired
