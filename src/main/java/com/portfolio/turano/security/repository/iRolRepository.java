369e4aa075aaeb7011a87b8d22ae2859445ad33c
Andres Turano <turamdq@gmail.com>
2022-08-26 00:16:16 -0300

    tercer commit

--- src/main/java/com/portfolio/turano/security/repository/iRolRepository.java
+++ src/main/java/com/portfolio/turano/security/repository/iRolRepository.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package com.portfolio.turano.security.repository;
 
 import com.portfolio.turano.security.entity.Rol;
@@ -6,8 +10,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ *
+ * @author turam
+ */
 @Repository
-public interface iRolRepository extends JpaRepository<Rol, Integer> {
-    
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
     Optional<Rol> findByRolNombre(RolNombre rolNombre);
 }
