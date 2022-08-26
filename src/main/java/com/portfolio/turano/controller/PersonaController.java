bb311c6b5a98d06f158b9c814613251049ff7595
Andres Turano <turamdq@gmail.com>
2022-08-26 00:03:54 -0300

    segundo commit

--- src/main/java/com/portfolio/turano/controller/PersonaController.java
+++ src/main/java/com/portfolio/turano/controller/PersonaController.java
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//ACLARO QUE ES UN CONTROLADOR CON LA ANNOTATION:
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 public class PersonaController {
@@ -24,44 +22,24 @@ public class PersonaController {
     @Autowired
     private IPersonaService persoServ;
     
-    //ANTE UNA SOLICITUD GET EN LA URL INDICADA
     @GetMapping ("/persona/ver")
     @ResponseBody
     public List<Persona> verPersonas () {
         return persoServ.verPersonas();    
     }
     
-    //ANTE UNA SOLICITUD POST EN LA URL INDICADA
-    //OBTENGO EL CONTENIDO DEL BODY Y LO GUARDO EN LA VARIABLE pers PARA GUARDAR
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping ("/persona/nueva")
     public void crearPersona (@RequestBody Persona per) {
         persoServ.crearPersona(per);    
-    }
+    }    
     
-    //ANTE UNA SOLICITUD DELETE EN LA URL INDICADA BORRO POR ID LA PERSONA
-    //CON PathVariable RECIBO EN LA URL UNA VARIABLE (ID)
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping ("/persona/borrar/{id}")
     public void borrarPersona (@PathVariable Long id) {
         persoServ.borrarPersona(id);
-    }
+    }    
     
-    //ANTE UNA SOLICITUD PUT EN LA URL INDICADA EDITO POR ID LA PERSONA
-    /*@PutMapping ("/persona/editar/{id}")
-    public Persona editarPersona (@PathVariable Long id,
-                               @RequestParam("name") String newName,
-                               @RequestParam("position") String newPosition,
-                               @RequestParam("about") String newAbout){
-        Persona per = persoServ.buscarPersona(id);
-        
-        per.setName(newName);
-        per.setPosition(newPosition);
-        per.setAbout(newAbout);
-        
-        persoServ.crearPersona(per);
-        return per;
-    }*/
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/persona/editar/{id}")
     public void editarPersona(@PathVariable("id") Long id, 
